7e970f2a7c3a34510dc4b715e45fc6974a52cd2d
Julia Winogrodzka <grazia.julia@gmail.com>
2020-12-28 16:11:55 +0100

    Using setState inside switchNameHandler

--- react-project-app/src/App.js
+++ react-project-app/src/App.js
@@ -8,7 +8,21 @@ class App extends Component {
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
       { name: "Julia", age: 23 }
-    ]
+    ],
+    otherState: "some other value"
+  }
+
+  switchNameHandler = () => {
+    //console.log('Was clicked!');
+    //DON'T DO THIS: this.state.persons[0].name = "Maximilian";
+
+    this.setState( {
+      persons: [
+      { name: "Maximilian", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Julia", age: 27 }
+      ]
+    } )
   }
 
   render() {
@@ -16,7 +30,7 @@ class App extends Component {
       <div className="App">
         <h1>Hi I am React App</h1>
         <p>This is really working</p>
-        <button>Switch name</button>
+        <button onClick={this.switchNameHandler}>Switch name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: sport</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
